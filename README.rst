5bbc4d3bb6489e05ddac8151dd5337189b89f4ce
joshua-x-7 <joshuaxia7@gmail.com>
2023-10-21 12:08:46 -0500

    Deletion

--- README.rst
+++
@@ -1,4 +0,0 @@
-GridWorkbench
-=======
-
-**GridWorkbench** is a Python-based package that holds data structures for power system analysis.
--- WB_Objects.py
+++
@@ -1,7 +0,0 @@
-from gridworkbench import GridWorkbench
-wb = GridWorkbench()  # create GWB object
-fileName = r"C:\Users\Joshua (work)\Documents\Research\PowerWorld Cases\Hawaii40_220906.pwb"  # PowerWorld case directory
-wb.open_pwb(fileName)  # open case
-wb.pwb_read_all(hush = True)  # read in case, turn off default printout
-
-
